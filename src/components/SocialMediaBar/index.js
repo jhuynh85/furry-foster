fe6059b3092b421bbc0717e076a5e1577a8480a2
Margaret Choi <margaret.j.li@gmail.com>
2018-06-16 22:38:55 -0700

    Add content to about page and link social media icons

--- src/components/SocialMediaBar/index.js
+++ src/components/SocialMediaBar/index.js
@@ -2,18 +2,24 @@ import React from "react";
 import "./SocialMediaBar.css";
 
 class SocialMediaBar extends React.Component {
-  render() {
-    return (
-      <div className="social-media-bar level title-case">
-        <div className="level-left">SHOW US YOUR FOSTERS! SHARE YOUR STORIES AND PICTURES</div>
-        <div className="level-right">
-          <i className="fa fa-facebook fa-2x" aria-hidden="true" />&nbsp;&nbsp;&nbsp;
-          <i className="fa fa-twitter fa-2x" aria-hidden="true" />&nbsp;&nbsp;&nbsp;
-          <i className="fa fa-instagram fa-2x" aria-hidden="true" />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="social-media-bar level title-case">
+                <div className="level-left">SHOW US YOUR FOSTERS! SHARE YOUR STORIES AND PICTURES</div>
+                <div className="level-right">
+                    <a href="http://www.facebook.com/furryfoster">
+                        <i className="fa fa-facebook fa-2x" aria-hidden="true" />&nbsp;&nbsp;&nbsp;
+                    </a>
+                    <a href="http://www.twitter.com/furryfoster">
+                        <i className="fa fa-twitter fa-2x" aria-hidden="true" />&nbsp;&nbsp;&nbsp;
+                    </a>
+                    <a href="http://www.instagram.com/furryfosterpets">
+                        <i className="fa fa-instagram fa-2x" aria-hidden="true" />
+                    </a>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default SocialMediaBar;
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,7 +3,30 @@ import "./About.css";
 
 class About extends React.Component {
   render() {
-    return <div>About page</div>;
+    return (      
+		<div>
+			<h1 class="title is-1">About Us</h1>
+			<div class="content">
+				<h2 class="subtitle is-2">Who We Are</h2>
+				<p>Furry Foster is a 501(c)3 federal non-profit organization dedicated to ending 
+				the euthanasia of adoptable pets by connecting them with loving foster families.</p>
+
+				<h2 class="subtitle is-2">What We Can Do</h2>
+				<p>We support local animal rescue organizations and shelters with our services.</p>
+
+				<h2 class="subtitle is-2">Why Our Cause Matters</h2>
+				<p>Sadly, over 4 Million Shelter Pets are euthanized every year. We believe foster 
+				families can change that.</p>
+
+				<p>9,000 shelter pets a day are killed, not because of aggression, 
+				behavior or illness but simply due to a lack of space and resources. 
+				By introducing animals lovers to fostering and joining forces with local rescues and shelters, 
+				many more animal lives can be saved.</p>
+
+				<p>We can't do it alone, but together we can bring every pet home.</p>
+			</div>
+		</div>
+	);
   }
 }
 
