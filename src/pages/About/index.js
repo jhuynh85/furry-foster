7f80b397935b4da54a407441a3453dbd5720749b
Margaret Choi <margaret.j.li@gmail.com>
2018-06-16 22:54:02 -0700

    Fix className

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -4,17 +4,17 @@ import "./About.css";
 class About extends React.Component {
   render() {
     return (      
-		<div>
-			<h1 class="title is-1">About Us</h1>
-			<div class="content">
-				<h2 class="subtitle is-2">Who We Are</h2>
+		<div className="container is-widescreen">
+			<h1 className="title is-1">About Us</h1>
+			<div className="content">
+				<h4 className="subtitle is-4">Who We Are</h4>
 				<p>Furry Foster is a 501(c)3 federal non-profit organization dedicated to ending 
 				the euthanasia of adoptable pets by connecting them with loving foster families.</p>
 
-				<h2 class="subtitle is-2">What We Can Do</h2>
+				<h4 className="subtitle is-4">What We Can Do</h4>
 				<p>We support local animal rescue organizations and shelters with our services.</p>
 
-				<h2 class="subtitle is-2">Why Our Cause Matters</h2>
+				<h4 className="subtitle is-4">Why Our Cause Matters</h4>
 				<p>Sadly, over 4 Million Shelter Pets are euthanized every year. We believe foster 
 				families can change that.</p>
 
