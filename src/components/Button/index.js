b5411ed3f0f3fd1c572b578438d032292feee799
Joseph Huynh <joe.huynh@gmail.com>
2018-06-20 18:00:40 -0700

    Added hover effect to buttons

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,14 +2,36 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Button.css";
 
-const Button = props => {
-  return (
-    <div className={props.className}>
-      <Link to={props.link} className="button is-warning">
-        <strong>{props.text}</strong>
-      </Link>
-    </div>
-  );
-};
+class Button extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			hover: false
+		};
+	}
+
+	handleMouseEnter = e => {
+		this.setState({ hover: true });
+	};
+
+	handleMouseLeave = e => {
+		this.setState({ hover: false });
+	};
+
+	render() {
+		return (
+			<div className={this.props.className}>
+				<Link
+					to={this.props.link}
+					ref="button"
+					className={this.state.hover ? "button is-warning is-focused" : "button is-warning"}
+					onMouseEnter={this.handleMouseEnter}
+					onMouseLeave={this.handleMouseLeave}>
+					<strong>{this.props.text}</strong>
+				</Link>
+			</div>
+		);
+	}
+}
 
 export default Button;
