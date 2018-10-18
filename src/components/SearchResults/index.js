1e744574cb30d106c1db4f7cd8ec1f9468660dc4
Margaret Choi <margaret.j.li@gmail.com>
2018-10-17 17:44:59 -0700

    Hover effect on search tiles

--- src/components/SearchResults/index.js
+++ src/components/SearchResults/index.js
@@ -11,25 +11,37 @@ class SearchResults extends React.Component {
 						className="tile is-parent is-3"
 						searchTileURL="/pets"
 						searchTileImage="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/42753794/1/?bust=1536929975&width=1439"
-						searchTileHeader="Platinum"
+						searchTileName="Platinum"
+						searchTileAge="5"
+						searchTileGender="female"
+						searchTileLocation="San Diego, CA"
 					/>
 					<SearchTile
 						className="tile is-parent is-3"
 						searchTileURL="/pets"
 						searchTileImage="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/42753794/1/?bust=1536929975&width=1439"
-						searchTileHeader="Platinum"
+						searchTileName="Platinum"
+						searchTileAge="5"
+						searchTileGender="female"
+						searchTileLocation="San Diego, CA"
 					/>
 					<SearchTile
 						className="tile is-parent is-3"
 						searchTileURL="/pets"
 						searchTileImage="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/42753794/1/?bust=1536929975&width=1439"
-						searchTileHeader="Platinum"
+						searchTileName="Platinum"
+						searchTileAge="5"
+						searchTileGender="female"
+						searchTileLocation="San Diego, CA"
 					/>
 					<SearchTile
 						className="tile is-parent is-3"
 						searchTileURL="/pets"
 						searchTileImage="https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/42753794/1/?bust=1536929975&width=1439"
-						searchTileHeader="Platinum"
+						searchTileName="Platinum"
+						searchTileAge="5"
+						searchTileGender="female"
+						searchTileLocation="San Diego, CA"
 					/>
 				</div>
 			</div>
--- src/components/SearchTile/index.js
+++ src/components/SearchTile/index.js
@@ -1,23 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
+import Button from "../Button";
 import "./SearchTile.css";
 
-const SearchTile = props => {
-	return (
-		<div className={props.className}>
-			<a className="searchtile-container" href={props.searchTileURL}>
+class SearchTile extends Component {
+	constructor(props) {
+		super(props);
+		this.handleMouseHover = this.handleMouseHover.bind(this);
+		this.state = {
+			isHovering: false
+		};
+	}
+
+	handleMouseHover() {
+		this.setState(this.toggleHoverState);
+	}
+
+	toggleHoverState(state) {
+		console.log("state", state.isHovering);
+		return {
+			isHovering: !state.isHovering
+		};
+	}
+
+	render() {
+		return (
+			<div className={this.props.className}>
 				<div
-					className="searchtile-image"
-					style={{
-						backgroundImage: "url(" + props.searchTileImage + ")",
-						backgroundRepeat: "no-repeat"
-					}}>
-					<div className="searchtile-overlay">
-						<p className="searchtile-header">{props.searchTileHeader}</p>
+					className="searchtile-container"
+					href={this.props.searchTileURL}
+					onMouseEnter={this.handleMouseHover}
+					onMouseLeave={this.handleMouseHover}>
+					<div
+						className="searchtile-image"
+						style={{
+							backgroundImage: "url(" + this.props.searchTileImage + ")",
+							backgroundRepeat: "no-repeat"
+						}}>
+						<div className="searchtile-overlay">
+							<div className="searchtile-header">
+								<div className="searchtile-name">{this.props.searchTileName}</div>
+								{this.state.isHovering && (
+									<div className="searchtile-details">
+										<p>
+											{this.props.searchTileAge} {this.props.searchTileGender}
+											<br />
+											{this.props.searchTileLocation}
+										</p>
+										<Button link="/" text="More Info" />
+									</div>
+								)}
+							</div>
+						</div>
 					</div>
 				</div>
-			</a>
-		</div>
-	);
-};
+			</div>
+		);
+	}
+}
 
 export default SearchTile;
